822e57cdbd9695e88331471795b452475480568e
Lars Michael <lmi@danskecommodities.com>
2023-08-22 12:45:26 +0200

    Added map component

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import { MarketDataMap } from './map/map';
 
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="market-data-map" />
+      <MarketDataMap />
     </div>
   );
 }
