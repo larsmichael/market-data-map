46bbd430d0c7f22f0e10372c1040b785315b65cf
Lars Michael <lmi@danskecommodities.com>
2023-09-28 14:25:28 +0200

    Introducing checkboxes

--- src/app/MarketDataMap/MarketDataMap.tsx
+++ src/app/MarketDataMap/MarketDataMap.tsx
@@ -17,8 +17,9 @@ const INITIAL_VIEW_STATE = {
   bearing: 0,
 };
 
-const colorScaleFunction = scaleThreshold<number, RGBAColor>().domain([0, 5, 10]).range(
-[ 
+const colorScaleFunction = scaleThreshold<number, RGBAColor>()
+.domain([0, 5, 10])
+.range([ 
   [255, 0, 0, 255],
   [255, 165, 0, 255],
   [255, 255, 0, 255],
@@ -27,10 +28,12 @@ const colorScaleFunction = scaleThreshold<number, RGBAColor>().domain([0, 5, 10]
 
 const MarketDataMap = () => {
   const [geoJsonData, setGeoJsonData] = useState(null);
+  const [layer1, setLayer1] = useState<boolean>(true);
+  const [layer2, setLayer2] = useState<boolean>(false);
 
   useEffect(() => {
     fetch(
-      'https://raw.githubusercontent.com/larsmichael/market-data-map/master/public/featurecollection-sample.json'
+      'https://raw.githubusercontent.com/larsmichael/market-data-map/master/public/featurecollection-sample-caiso.json'
     )
       .then((response) => response.json())
       .then((data) => {
@@ -42,37 +45,76 @@ const MarketDataMap = () => {
   }, []);
 
   return (
-    <DeckGL
-      initialViewState={INITIAL_VIEW_STATE}
-      controller={true}
-      layers={[
-        new GeoJsonLayer({
-          id: 'geojson-layer',
-          data: geoJsonData,
-          pickable: true,
-          lineWidthScale: 5,
-          lineWidthMinPixels: 2,
-          pointRadiusScale: 4,
-          getFillColor: (d: any) => colorScaleFunction(d.properties.priceDiff),
-          getLineColor: (d: any) => colorScaleFunction(d.properties.priceDiff),
-          getPointRadius: 100,
-          getElevation: 300,
-        }) as any,
-      ]}
-      getTooltip={({object}) => object && object.properties.marketArea + '\n' +
-        object.properties.name + '\n' +
-        'DayAhead: $' +  (Math.round(object.properties.dayAheadPrice * 100) / 100).toFixed(2) + '\n' +
-        'RealTime: $' +  (Math.round(object.properties.realTimePrice * 100) / 100).toFixed(2) + '\n' +
-        'Diff: $' +  (Math.round(object.properties.priceDiff * 100) / 100).toFixed(2)
-      }
-    >
-      <Map
-        mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
-        mapStyle="mapbox://styles/mapbox/light-v9"
-      />
-    </DeckGL>
+    <>
+      <label style={{ fontSize: 14, fontFamily: "Helvetica", position: 'fixed', top: 11, left: 35, zIndex: 10000 }}>
+        <input
+          type="checkbox"
+          style={{ position: 'fixed', top: 10, left: 10, zIndex: 10000 }}
+          onChange={(event) => setLayer1(event.currentTarget.checked)}
+        ></input>CAISO
+      </label>
+      <label style={{ fontSize: 14, fontFamily: "Helvetica", position: 'fixed', top: 31, left: 35, zIndex: 10000 }}>
+        <input
+          type="checkbox"
+          style={{ position: 'fixed', top: 30, left: 10, zIndex: 10000 }}
+          onChange={(event) => setLayer2(event.currentTarget.checked)}
+        ></input>ISONE
+      </label>
+      <DeckGL
+        initialViewState={INITIAL_VIEW_STATE}
+        controller={true}
+        layers={[
+          ...(layer1
+            ? [
+                new GeoJsonLayer({
+                  id: 'geojson-layer',
+                  data: geoJsonData,
+                  pickable: true,
+                  lineWidthScale: 5,
+                  lineWidthMinPixels: 2,
+                  pointRadiusScale: 0.5,
+                  getFillColor: (d: any) =>
+                    colorScaleFunction(d.properties.priceDiff),
+                  getLineColor: (d: any) =>
+                    colorScaleFunction(d.properties.priceDiff),
+                  getPointRadius: 100,
+                  getElevation: 300,
+                }) as any,
+              ]
+            : []),
+          ...(layer2
+            ? [
+                new GeoJsonLayer({
+                  id: 'geojson-layer',
+                  data: geoJsonData,
+                  pickable: true,
+                  lineWidthScale: 5,
+                  lineWidthMinPixels: 2,
+                  pointRadiusScale: 0.5,
+                  getFillColor: (d: any) =>
+                    colorScaleFunction(d.properties.priceDiff),
+                  getLineColor: (d: any) =>
+                    colorScaleFunction(d.properties.priceDiff),
+                  getPointRadius: 100,
+                  getElevation: 300,
+                }) as any,
+              ]
+            : []),
+        ]}
+        getTooltip={({object}) => object && object.properties.marketArea + '\n' +
+          object.properties.name + '\n' +
+          'DayAhead: $' +  (Math.round(object.properties.dayAheadPrice * 100) / 100).toFixed(2) + '\n' +
+          'RealTime: $' +  (Math.round(object.properties.realTimePrice * 100) / 100).toFixed(2) + '\n' +
+          'Diff: $' +  (Math.round(object.properties.priceDiff * 100) / 100).toFixed(2)
+        }
+      >
+        <Map
+          mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
+          mapStyle="mapbox://styles/mapbox/light-v9"
+        />
+      </DeckGL>
+    </>
   );
 };
-
 
 export { MarketDataMap };
